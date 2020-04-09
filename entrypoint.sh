49ea5d54b7b8ef49f9bd8a2c7cf6367de7969f58
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2020-04-09 16:45:41 -0300

    add extension point in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,15 @@ then
     ln -sf "${SYSLOG_SOCK}" /dev/log
 fi
 
+if [ -d "/entrypoint.d" ];
+then
+    for extra in /entrypoint.d/*; do
+        case "$extra" in
+            *.sh)     . "$extra" ;;
+        esac
+        echo
+    done
+    unset extra
+fi
+
 exec "$@"
